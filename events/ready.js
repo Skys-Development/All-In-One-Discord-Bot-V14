43da1a46eacfe0ad389815e9b3f56a83aa259eaa
William Holley <william2sober@gmail.com>
2025-05-11 17:47:28 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -5,5 +5,9 @@ module.exports = {
   once: true,
   execute(client) {
     console.log(magenta('💖 Ready'), green('•'), cyan(`Logged in as `) + bold(client.user.tag));
+    client.user.setPresence({
+      activities: [{ name: `${client.guilds.cache.size} servers`, type: 3 }],
+      status: 'online'
+    });
   }
 };
